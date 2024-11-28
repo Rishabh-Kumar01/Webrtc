c2a7bcbc8adf915f8426a631b1e0f337e809f59c
Rishabh-Kumar01 <rishabh1teotia@gmail.com>
2024-11-28 19:39:37 +0530

    chore: Add EventsGateway to AppModule providers

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,24 @@
+import { OnModuleInit } from '@nestjs/common';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+})
+export class EventsGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log('New user connected');
+
+      socket.on('disconnect', () => {
+        console.log('User disconnected');
+      });
+    });
+  }
+}
